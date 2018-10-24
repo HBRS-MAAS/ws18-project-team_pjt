4f7b332a8c4899ef92b4947b2122c28b4a28aa89
janl1992 <s124115@hft-leipzig.de>
2018-10-24 11:19:55 +0200

    Testcommit

--- src/main/java/org/yourteamname/agents/DummyAgent.java
+++ src/main/java/org/yourteamname/agents/DummyAgent.java
@@ -15,7 +15,7 @@ import jade.lang.acl.ACLMessage;
 public class DummyAgent extends Agent {
 	protected void setup() {
 	// Printout a welcome message
-		System.out.println("Hello! Dummy-agent "+getAID().getName()+" is ready.");
+		System.out.println("Hello! Dummy-agent "+getAID().getName()+" is ready. test");
 
         try {
  			Thread.sleep(3000);
