e2bce50c66f3348b12b46568a23d2e5bee10a38a
Dr4gonbl4de <3546GtRf>
2018-11-01 11:24:59 +0100

    added method for parsing arguments

--- src/main/java/org/team_pjt/Start.java
+++ src/main/java/org/team_pjt/Start.java
@@ -4,9 +4,16 @@ import java.util.List;
 import java.util.Vector;
 
 public class Start {
+	private static boolean isHost;
+	private static String host;
+	private static String port;
+
     public static void main(String[] args) {
+    	if (!decodeArguments(args)) {
+    		return;
+		}
     	List<String> agents = new Vector<>();
-    	agents.add("test:org.team_pjt.agents.DummyAgent");
+    	agents.add("test:org.team_pjt.agents.SystemClockAgent");
 
     	List<String> cmd = new Vector<>();
     	cmd.add("-agents");
@@ -18,4 +25,30 @@ public class Start {
     	cmd.add(sb.toString());
         jade.Boot.main(cmd.toArray(new String[cmd.size()]));
     }
+
+    private static boolean decodeArguments(String[] args) {
+    	for (int i = 0; i < args.length; ++i) {
+    		if (args[i].equals("isHost")) {
+    			isHost = true;
+    			continue;
+			}
+			if (args[i].equals("-host")) {
+				host = args[i+1];
+				++i;
+			}
+			if (args[i].equals("-port")) {
+				port = args[i+1];
+				++i;
+			}
+			if (args[i].equals("-h")) {
+				// TODO: implement help output
+				System.out.println("");
+			}
+		}
+		if (!isHost && (port == null || host == null)) {
+			System.out.println("if instance id not host, host and port have to be specified!");
+			return false;
+		}
+		return true;
+	}
 }
