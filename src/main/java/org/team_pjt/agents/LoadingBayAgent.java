d1cfdaf92158fb5b996127109a79c87813c7ffff
Pascal Maczey <Pascalmaczey@web.de>
2018-11-11 14:23:30 +0100

    Implementing skeleton of common agents

---
+++ src/main/java/org/team_pjt/agents/LoadingBayAgent.java
@@ -0,0 +1,9 @@
+package org.team_pjt.agents;
+
+import jade.core.AID;
+import jade.core.Agent;
+
+public class LoadingBayAgent extends Agent {
+    private String bakery_id;
+    private AID scheduler_agent;
+}
