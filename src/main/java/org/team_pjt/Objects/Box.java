fb69755fbfecb8b4da474e07b5e0bdbf8bc8ac8c
Pascal Maczey <Pascalmaczey@web.de>
2019-01-18 22:11:06 +0100

    deleted unused files

--- src/main/java/org/team_pjt/Objects/Box.java
+++
@@ -1,6 +0,0 @@
-package org.team_pjt.Objects;
-
-public class Box {
-    private int iCapacity;
-    private boolean bIsLoaded;
-}
--- src/main/java/org/team_pjt/Objects/Edges.java
+++
@@ -1,8 +0,0 @@
-package org.team_pjt.Objects;
-
-public class Edges {
-    String sSource;
-    String sTarget;
-    String sGuid;
-    float fDist;
-}
