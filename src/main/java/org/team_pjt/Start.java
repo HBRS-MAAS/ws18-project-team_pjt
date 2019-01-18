b35e9c68d2e001580089ad652b101c282ea2014a
Pascal Maczey <Pascalmaczey@web.de>
2019-01-18 21:32:57 +0100

    updated Start class according to upstream

--- src/main/java/org/team_pjt/Start.java
+++ src/main/java/org/team_pjt/Start.java
@@ -2,7 +2,6 @@ package org.team_pjt;
 
 import java.util.List;
 import java.util.Vector;
-import org.team_pjt.OrderProcessingInitializer;
 
 public class Start {
     private static boolean isHost = true;
@@ -17,9 +16,9 @@ public class Start {
     private static boolean packagingStage = false;
     private static boolean deliveryStage = false;
     private static boolean visualizationStage = false;
-    private static boolean noAgentStarting = false;
+    private static boolean noAgentStarting = true;
 
-    private static String endTime = "030.00.00";
+    private static String endTime = "030.03.00";
     private static String scenarioDirectory = "small";
 
     public static void main(String[] args) {
@@ -28,12 +27,15 @@ public class Start {
         }
 
         List<String> cmd = buildCMD();
-        jade.Boot.main(cmd.toArray(new String[cmd.size()]));
+        jade.Boot.main(cmd.toArray(new String[0]));
     }
 
-    public static List<String> buildCMD() {
+    private static List<String> buildCMD() {
         StringBuilder sb = new StringBuilder();
         List<String> cmd = new Vector<>();
+
+        cmd.add("-jade_domain_df_maxresult");
+        cmd.add("10000");
 
         if(isHost) {
             cmd.add("-local-port");
@@ -51,10 +53,9 @@ public class Start {
         if(customerStage) {
             Initializer init = new CustomerInitializer();
             sb.append(init.initialize(scenarioDirectory));
-//            endTime = "000.06.00";
         }
         if(orderProcessingStage) {
-			Initializer init = new OrderProcessingInitializer();
+            Initializer init = new OrderProcessingInitializer();
             sb.append(init.initialize(scenarioDirectory));
         }
         if(doughPrepStage) {
@@ -62,27 +63,20 @@ public class Start {
             sb.append(init.initialize(scenarioDirectory));
         }
         if(bakingStage) {
-//			Initializer init = new BakingStageInitializer();
-//            sb.append(init.initialize());
-//            endTime = "000.06.00";
         }
         if(packagingStage) {
-//			Initializer init = new PackagingStageInitializer();
-//            sb.append(init.initialize());
-//            endTime = "000.11.00";
         }
         if(deliveryStage) {
 
         }
         if(visualizationStage) {
-
         }
-		if(isHost) {
-			sb.append("timekeeper:org.team_pjt.agents.TimeKeeper(" + scenarioDirectory + ", " + endTime + ");");
-			if(noAgentStarting) {
-			    sb.append("dummy:org.team_pjt.agents.DummyAgent;");
+        if(isHost) {
+            sb.append("timekeeper:org.team_pjt.agents.TimeKeeper(" + scenarioDirectory + ", " + endTime + ");");
+            if(noAgentStarting) {
+                sb.append("dummy:org.team_pjt.agents.DummyAgent;");
             }
-		}
+        }
         cmd.add(sb.toString());
         return cmd;
     }
@@ -91,9 +85,11 @@ public class Start {
         for (int i = 0; i < args.length; ++i) {
             if (args[i].equals("-isHost")) {
                 isHost = true;
-                continue;
+                host = args[i+1];
+                ++i;
             }
             if (args[i].equals("-host")) {
+                isHost = false;
                 host = args[i+1];
                 ++i;
             }
@@ -134,17 +130,10 @@ public class Start {
                 noAgentStarting = false;
             }
             if (args[i].equals("-h")) {
-                System.out.println("Usage: ");
-                System.out.println("-----------------");
-                System.out.println("-container \t Specify container");
-                System.out.println("-ishHost \t Start the process as host");
-                System.out.println("-host \t Running in client mode - Set IP of host");
-                System.out.println("-port \t Running in client mode - Set port of host");
-                System.out.println("-----------------");
-                System.out.println("Example for using a client with the host running on the local machine");
-                System.out.println("java Start  -host 127.0.0.1 -port 8133");
-
+                // TODO: implement help output
+                System.out.println();
             }
+
         }
         if (!isHost && (port == null || host == null)) {
             System.out.println("instance is not host and host and port have to be specified!");
